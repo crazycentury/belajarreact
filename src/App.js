1d172b789168d9becd10f66a68a733506b558338
Iqbal Muflihuddin <crazycentury001@gmail.com>
2022-09-22 07:03:47 +0700

    tugas1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{ useState, useRef } from 'react';
+
+function FuncComp(){
+    const [count, setCount] = useState(0)
+    let count2 = 0
+
+    
+    return(
+      <div>
+        <h1>Halo Semua</h1>
+      </div>
+    )
+}
+
+const Timer = () => {
+  const [timer, setTimer] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+  const [isPaused, setIsPaused] = useState(false)
+  const countRef = useRef(null)
+
+  const Start = () =>{
+    setIsActive(true)
+    setIsPaused(true)
+    countRef.current = setInterval(()=> {
+      setTimer((timer) => timer+1)
+    },1000)
+  }
+
+  const Pause = () =>{
+    clearInterval(countRef.current)
+    setIsPaused(false)
+  }
+  
+  const Resume = () =>{
+    setIsPaused(true)
+    countRef.current = setInterval(() => {
+    setTimer((timer) => timer + 1)
+  }, 1000)
+  }
+
+  const Reset = () =>{
+    clearInterval(countRef.current)
+    setIsActive(false)
+    setIsPaused(false)
+    setTimer(0)
+  }
+
+  const formatTime = () => {
+    const getSeconds = `0${(timer % 60)}`.slice(-2)
+    const minutes = `${Math.floor(timer / 60)}`
+    const getMinutes = `0${minutes % 60}`.slice(-2)
+    const getHours = `0${Math.floor(timer / 3600)}`.slice(-2)
+
+    return `${getHours} : ${getMinutes} : ${getSeconds}`
+  }
+
+  return (
+    <div>
+      <h3>React Stopwatch</h3>
+      <div>
+        <div className='stopwatch-card'>
+          <p>{formatTime()}</p>
+          <p>Jam : Menit : Detik</p>
+        </div>
+        <div className='button-container'>
+          <button className="style-start" onClick={Start}>Start</button>
+          <button className="style-pause" onClick={Pause}>Pause</button> 
+          {/* <button onClick={Resume}>Resume</button> */}
+          <button className="style-reset" onClick={Reset}>Reset</button>
+        </div>
+      </div>
+
+      {/* Task2 */}
+      <div className='container-shape'>
+        <div className='container-column'>
+
+          <div className='container-top'>
+            <div className='sequere'/>
+            <div className='rectenggel'/>
+          </div>
+          <div className='container-top'>
+            <div className='sequere-border'/>
+            <div className='oval'/>
+          </div>
+          
+            <div className='sequere-green'/>
+          
+          
+          
+        </div>
+        <div className='green-rectenggel'/>
+        
+        
+
+      </div>
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Timer/>
     </div>
   );
 }
